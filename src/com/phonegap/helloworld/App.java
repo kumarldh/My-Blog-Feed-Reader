6fb9281d502c32af762536c41f6fd6721362969d
Kumar Chetan Sharma <kumarldh@gmail.com>
2012-01-14 18:50:38 +0530

    initial project commit, has all the files and code, should not require further checkins

---
+++ src/com/phonegap/helloworld/App.java
@@ -0,0 +1,14 @@
+package com.phonegap.helloworld;
+
+//import android.app.Activity;
+import com.phonegap.*;
+import android.os.Bundle;
+
+public class App extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
